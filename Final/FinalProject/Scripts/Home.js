744fc2c9ee9e6e1fc2aa2311b5c20228d2e82ab7
GodofBakon <ssutila@yahoo.com>
2016-12-15 22:28:06 -0500

    1

---
+++ Final/FinalProject/Scripts/Home.js
@@ -0,0 +1,45 @@
+﻿
+function loginFunction () {
+    $.ajax({
+        url: "Home/Login",
+        data: { 
+            Username: $(".username").val(),
+            Password: $(".password").val() 
+        },
+        success: function (result) { alert(result); }
+    });
+}
+function createAccountFunction() {
+    $.ajax({
+        url: "Home/CreateAccount",
+        data: { 
+            Username: $(".username").val(),
+            Password: $(".password").val(),
+            EmailAdd: $(".emailAdd").val(),
+            EmailCon: $(".emailCon").val()
+        },
+        success: function (result) { alert(result); }
+    });
+}
+function getAccountInfoFuction() {
+    $.ajax({
+        url: "Home/GetAccountInformation",
+        data: { Username: $(".username").val() },
+        success: function (result) { alert(result); }
+    });
+}
+function addorUpdateFuction() {
+    $.ajax({
+        url: "Home/AddorUpdateElement",
+        data: { 
+            Username: $(".username").val(),
+            Password: $(".password").val(),
+            ElementName: $(".elementName").val(),
+            ElementValue: $(".elementValue").val()
+        },
+        success: function (result) { alert(result); }
+    })
+}
+function change() {
+
+}
